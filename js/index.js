908478d7a9436c98f81a1351d4782aeaf1838e6d
farid <farid.mouzoun@outlook.fr>
2023-12-20 10:03:33 +0100

    exo 14
    fini

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var nbr = 1;
+while (nbr < 50 || nbr > 100) {
+    var nbr = parseInt(prompt("Veuillez saisir un nombre entre 50 et 100 ! "));
+    
+}
+alert("Merci pour votre coopération, vous avez chosi le numéro " + nbr);
